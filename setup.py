976bf5fdbbe7aa9ab33ebeebc9309da1a104d96a
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2025-10-25 18:54:21 +0200

    Adding dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup
 
-setup(name='pyctl',
-      version='0.2',
-      description='Control with Python',
-      url='https://github.com/mtguerreiro/pyctl.git',
-      author='Marco Guerreiro',
-      author_email='marcotulio.guerreiro@gmail.com',
-      packages=['pyctl'],
-      )
+setup(
+    name='pyctl',
+    version='0.2.1',
+    description='Control with Python',
+    url='https://github.com/mtguerreiro/pyctl.git',
+    author='Marco Guerreiro',
+    author_email='marcotulio.guerreiro@gmail.com',
+    packages=['pyctl'],
+    install_requires=[
+      'numpy',
+      'scipy',
+      'matplotlib',
+      'qpsolvers[quadprog]',
+      'osqp==0.6.7.post3',
+    ],
+)
