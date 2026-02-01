a506e9d4bf3231887c036e8d9c8a3c3c683e787e
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2026-02-01 17:33:00 +0100

    Simplifying boost cascaded feedback lin controller design

--- pyctl/design/pe/boost.py
+++ pyctl/design/pe/boost.py
@@ -6,16 +6,13 @@ from . import energy as _energy
 
 def casc_fblin(ts, pct_os, L, C, alpha=10):
 
-
-    zeta_i, wn_i = design_utils.zeta_wn(ts / alpha, pct_os)
-    k_ei = - L * wn_i**2
-    ki  =   2 * L * zeta_i * wn_i
+    k_ei = L * 4 / (ts / alpha)
 
     zeta_v, wn_v = design_utils.zeta_wn(ts, pct_os)
     k_ev = - C * wn_v**2
     kv  =   2 * C * zeta_v * wn_v
     
-    return (ki, k_ei, kv, k_ev)
+    return (k_ei, kv, k_ev)
 
 
 def energy(ts, pct_os, alpha=5):
