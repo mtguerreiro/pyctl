b99e04ca5615448a2e9788300bac3b00a6ab2e23
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2026-02-19 19:48:18 +0100

    Adding mpc energy design

--- pyctl/design/pe/energy.py
+++ pyctl/design/pe/energy.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.signal
+import pyctl
 
 from . import design_utils
 
@@ -26,3 +27,20 @@ def gains(ts, pct_os, alpha=5):
     k_ey = K[2]
     
     return (ky, k_y_dot, k_ey)
+
+
+def mpc_gains(rw, l_pred, dt, alpha=1.0):
+    
+    Am = np.array([[0.0, 1.0],
+                   [0.0, 0.0]])
+
+    Bm = np.array([[0.0],
+                   [alpha]])
+
+    Cm = np.array([1.0, 0.0])
+
+    Ad, Bd, Cd, _, _ = scipy.signal.cont2discrete((Am, Bm, Cm, 0), dt, method='zoh')
+
+    sys = pyctl.mpc.System(Ad, Bd, Cd, l_pred=l_pred, rw=rw)
+
+    return (sys.Kx, sys.Ky)
